6c0f17b07b250bf5292655bd423c9e519770c3e2
pyash2011-ui <pyash2011@gmail.com>
2026-07-28 15:27:56 +0530

    5th commit by Yash

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Sample project - Yash");
+        System.out.println("2nd trial by Yash");
+        System.out.println("Executing the code");
 
 
     }
